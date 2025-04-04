78a693dc7193a55e16f5caf10420df5b24450bee
Bozhidar <mrarthur0507@gmail.com>
2025-04-04 21:21:58 +0300

    db context and database setup

--- RealTimeChat.Presentation/Program.cs
+++ RealTimeChat.Presentation/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RealTimeChat.Infrastructure.ORM;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ChatContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"), x =>
+    {
+        x.MigrationsAssembly("RealTimeChat.Infrastructure");
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
