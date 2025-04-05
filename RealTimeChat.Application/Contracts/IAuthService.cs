cc7d2f7b60222f8e4e1ee4ad07bc9444d9b3ba38
Bozhidar <mrarthur0507@gmail.com>
2025-04-05 17:12:55 +0300

    added login/registration logic

---
+++ RealTimeChat.Application/Contracts/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeChat.Application.Contracts
+{
+    public interface IAuthService
+    {
+        public Task<bool> RegisterAsync(string username, string email, string password);
+
+        public Task<bool> LoginAsync(string username, string password);
+    }
+}
---
+++ RealTimeChat.Infrastructure/Implementations/Auth/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RealTimeChat.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeChat.Infrastructure.Implementations.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthService(UserManager<IdentityUser> userManager) {
+            _userManager = userManager;
+        }
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null || ! await _userManager.CheckPasswordAsync(user, password))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<bool> RegisterAsync(string username, string email, string password)
+        {
+            var user = new IdentityUser { UserName = username, Email = email };
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
